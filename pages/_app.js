96622fedbe4041ebbad6906ad610747bb2d82da5
lokch05 <lokch0599@gmail.com>
2022-02-04 22:40:03 +0800

    update pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+import * as React from "react";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+import Navbar from "../src/Components/Navbar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  );
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Navbar />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
